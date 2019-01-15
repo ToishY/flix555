ba7c2db94f04bf5427bbd4b0045d773756800038
ZteeY <31921460+ZteeY@users.noreply.github.com>
2019-01-15 22:02:24 +0100

    Update flixxClass.php

--- flixxClass.php
+++ flixxClass.php
@@ -40,78 +40,78 @@
 		}
 
 		function accountInfo(){
-			return 'http://'.$this->host.'/api/account/info?%key%';
+			return $this->host.'/api/account/info?%key%';
 		}
 
 		function accountStats($daysLimit = 14){
-			return 'http://'.$this->host.'/api/account/stats?%key%&last='.$daysLimit;
+			return $this->host.'/api/account/stats?%key%&last='.$daysLimit;
 		}
 
 		function uploadByURL($videoURL){
-			return 'http://'.$this->host.'/api/upload/url?%key%&url='.$videoURL;
+			return $this->host.'/api/upload/url?%key%&url='.$videoURL;
 		}
 
 		function uploadServer(){
-			return 'http://'.$this->host.'/api/upload/server?%key%';
+			return $this->host.'/api/upload/server?%key%';
 		}
 
 		function fileInfo($fileCodes){
-			return 'http://'.$this->host.'/api/file/info?%key%&file_code='.urlencode($fileCodes);
+			return $this->host.'/api/file/info?%key%&file_code='.urlencode($fileCodes);
 		}
 
 		function fileList($inputArray){
 			//$inputArray = array("a1b2c3e4",1,100,0,NULL,NULL);
 			list($folderId, $pageNumber, $perPage, $public, $created, $videoTitle) = $inputArray;
-			return 'http://'.$this->host.'/api/file/list?%key%&page='.$pageNumber.'&per_page='.$perPage.'&fld_id='.$folderId.'&public='.$public.'&created='.urlencode($created).'&title='.urlencode($videoTitle);
+			return $this->host.'/api/file/list?%key%&page='.$pageNumber.'&per_page='.$perPage.'&fld_id='.$folderId.'&public='.$public.'&created='.urlencode($created).'&title='.urlencode($videoTitle);
 		}
 
 		function fileRename($inputArray){
 			//$inputArray = array("d5e6f7g8","my awesome video")
 			list($fileCodes, $videoTitle) = $inputArray;
-			return 'http://'.$this->host.'/api/file/rename?%key%&file_code='.urlencode($fileCodes).'&title='.urlencode($videoTitle);
+			return $this->host.'/api/file/rename?%key%&file_code='.urlencode($fileCodes).'&title='.urlencode($videoTitle);
 		}
 
 		function fileClone($fileCodes){
-			return 'http://'.$this->host.'/api/file/clone?%key%&file_code='.urlencode($fileCodes);
+			return $this->host.'/api/file/clone?%key%&file_code='.urlencode($fileCodes);
 		}
 
 		function fileSetFolder($inputArray){
 			//$inputArray = array("d5e6f7g8","folderName")
 			list($fileCodes, $folderId) = $inputArray;
-			return 'http://'.$this->host.'/api/file/set_folder?%key%&file_code='.urlencode($fileCodes).'&fld_id='.$folderId;
+			return $this->host.'/api/file/set_folder?%key%&file_code='.urlencode($fileCodes).'&fld_id='.$folderId;
 		}
 
 		function folderList($folderId){
-			return 'http://'.$this->host.'/api/folder/list?%key%&fld_id='.$folderId;
+			return $this->host.'/api/folder/list?%key%&fld_id='.$folderId;
 		}
 
 		function createFolder($inputArray){
 			//$inputArray = array("xle1390a","my awesome folder")
 			list($parentId, $folderName) = $inputArray;
-			return 'http://'.$this->host.'/api/folder/create?%key%&parent_id='.$parentId.'&name='.urlencode($folderName);
+			return $this->host.'/api/folder/create?%key%&parent_id='.$parentId.'&name='.urlencode($folderName);
 		}
 
 		function renameFolder($inputArray){
 			//$inputArray = array("a1b2c3e4","my awesome folder")
 			list($folderId, $folderName) = $inputArray;
-			return 'http://'.$this->host.'/api/folder/rename?%key%&fld_id='.$folderId.'&name='.urlencode($folderName);
+			return $this->host.'/api/folder/rename?%key%&fld_id='.$folderId.'&name='.urlencode($folderName);
 		}
 
 		function deletedFiles($fileLimit = 50){
-			return 'http://'.$this->host.'/api/files/deleted?%key%&last='.$fileLimit;
+			return $this->host.'/api/files/deleted?%key%&last='.$fileLimit;
 		}
 
 		function dmcaFiles($fileLimit = 50){
-			return 'http://'.$this->host.'/api/files/dmca?%key%&last='.$fileLimit;
+			return $this->host.'/api/files/dmca?%key%&last='.$fileLimit;
 		}
 
 		/*
-		#This call did not work > resulted in forbidden
+		#This call did not work; resulted in forbidden / 403
 		function fileDirectLink($fileCode, $quality = NULL){
 			if((isset($quality)) && ($quality === "h" || $quality === "l" || $quality === "o"){
-				return 'http://'.$this->host.'/api/file/direct_link?%key%&file_code='.$fileCode.'&q='.$quality;
+				return $this->host.'/api/file/direct_link?%key%&file_code='.$fileCode.'&q='.$quality;
 			}
-			return 'http://'.$this->host.'/api/file/direct_link?%key%&file_code='.$fileCode;
+			return $this->host.'/api/file/direct_link?%key%&file_code='.$fileCode;
 		}
 		*/
 	}
