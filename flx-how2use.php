d5a07732e87cc6fdb47f927188bd0f018c20d803
ZteeY <31921460+ZteeY@users.noreply.github.com>
2019-01-15 22:01:02 +0100

    Update flx-how2use.php

--- flx-how2use.php
+++ flx-how2use.php
@@ -3,7 +3,7 @@
 require_once 'flixxClass.php';
 
 # Pass your API key from the FLIXX555 user panel in here
-$flx = new FlixxMain("a1b2c3e4f5g6h7i8j9");
+$flx = new FlixxMain("http://flix555.com","a1b2c3e4f5g6h7i8j9");
 
 # Get account info
 $res = $flx->curlBuilder("accountInfo");
@@ -11,7 +11,7 @@ print_r($res);
 
 # Account statistics
 /* Arguments:
-- Optional: 
+- Optional
 	Days; amount of days for statistics; default 14
 */
 $res = $flx->curlBuilder("accountStats",7);
@@ -19,7 +19,7 @@ print_r($res);
 
 # Upload file by URL 
 /* Arguments:
-- Required: 
+- Required
 	URL; direct link of video url
 */
 $res = $flx->curlBuilder("uploadByURL","http://clips.vorwaerts-gmbh.de/VfE_html5.mp4");
@@ -31,7 +31,7 @@ print_r($res);
 
 # Get file info
 /* Arguments:
-- Required: 
+- Required
 	FileCode(s); if multiple files, please comma seperate
 */
 $res = $flx->curlBuilder("fileInfo","d5e6f7g8,h9i10j11k");
@@ -39,12 +39,12 @@ print_r($res);
 
 # Get file list
 /* Arguments (in array):
-- Required:
+- Required
 	FolderId; folder id; 0 = root
 	PageNumber; page number
 	PerPage; amount of items displayed per page
 	Public; 0 for private files / 1 for public file
-- Optional 
+- Optional
 	Created; filter files only after this time e.g. "2018-06-21 05:07:10"
 	Title; filter on video title e.g. "Iron Man"
 */
@@ -53,8 +53,8 @@ print_r($res);
 
 # Rename files
 /* Arguments (in array):
-- Required:
-	FileCodes; if multiple files, please comma seperate
+- Required
+	FileCodes;
 	VideoTitle; the new title of the files specified by Filecodes
 */
 $res = $flx->curlBuilder("fileRename",array("d5e6f7g8,h9i10j11k","my awesome videos V2"));
@@ -62,16 +62,16 @@ print_r($res);
 
 # Clone files
 /* Arguments:
-- Required:
-	FileCodes; if multiple files, please comma seperate
+- Required
+	FileCodes;
 */
 $res = $flx->curlBuilder("fileClone","d5e6f7g8,h9i10j11k");
 print_r($res);
 
 # Set folder for file
 /* Arguments (in array):
-- Required:
-	FileCodes; if multiple files, please comma seperate
+- Required
+	FileCodes
 	FolderId; folder id the files go to; 0 = root
 */
 $res = $flx->curlBuilder("fileSetFolder",array("d5e6f7g8,h9i10j11k",123);
@@ -79,15 +79,15 @@ print_r($res);
 
 # Get folder/file list
 /* Arguments:
-- Required:
-	FolderId; folder id; 0 = root
+- Required
+	FolderId;
 */
 $res = $flx->curlBuilder("folderList","a1b2c3e4");
 print_r($res);
 
 # Create folder
 /* Arguments (in array):
-- Required:
+- Required
 	ParentId; the id of the parent folder; 0 = root
 	FolderName; the name of the folder
 */
@@ -96,7 +96,7 @@ print_r($res);
 
 # Rename folder
 /* Arguments (in array):
-- Required:
+- Required
 	FolderId; folder id of the folder you want to rename
 	FolderName; the new name of the folder
 */
@@ -105,7 +105,7 @@ print_r($res);
 
 # Deleted files
 /* Arguments:
-- Optional:
+- Optional
 	FileLimit; amount of files you want to display; default 50
 */
 $res = $flx->curlBuilder("deletedFiles",25);
@@ -113,8 +113,8 @@ print_r($res);
 
 # DMCA files
 /* Arguments:
-- Optional:
-	FileLimit; amount of files you want to display; default 50
+- Optional
+	FileLimit;
 */
 $res = $flx->curlBuilder("dmcaFiles",25);
 print_r($res);
